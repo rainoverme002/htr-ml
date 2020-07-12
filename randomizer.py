eb4072f8e10c5ed61fc18b7ceec243df40eb55cd
whitequbits <bagus.gitdata@gmail.com>
2020-07-12 15:30:27 +0700

    Add title replacer and better randomizer

--- randomizer.py
+++ randomizer.py
@@ -4,9 +4,10 @@ import os
 DIRECTORY = 'result'
 BASE_COORDINATE = -351.818
 HTR_FUEL_HEIGHT_LINE = 267
+TITLE_LINE = 3
 
 def generate_random_number(total_random_number):
-    return random.sample(range(0, 190), total_random_number)
+    return random.sample(range(80, 220), total_random_number)
 
 
 def replace_specific_line(line_number, read_file_name, line_content, write_file_name, save_path=DIRECTORY):
@@ -30,7 +31,20 @@ def htr_fuel_height_replacer(height, read_file_name):
     replace_specific_line(HTR_FUEL_HEIGHT_LINE, read_file_name,
                           fuel_height_line_content, write_file_name)
 
-    print(f"Succesfully create input file for {height}")
+    print(f"Succesfully create input file for {height} cm")
+
+    return write_file_name
+
+
+def title_replacer(height, read_file_name):
+    write_file_name = read_file_name
+    read_file_name = os.path.join(DIRECTORY, write_file_name)
+
+    fuel_height_line_content = f"set title HTR-10 - Helium - {height} cm"
+
+    replace_specific_line(TITLE_LINE, read_file_name,
+                          fuel_height_line_content, write_file_name)
+
 
 
 def create_result_directory():
@@ -43,18 +57,20 @@ def generator():
     create_result_directory()
 
     answer = input("Do you want randomize the height? (y/n) ")
-    read_file_name = input("Please input your basis file name = ")
+    basis_file_name = input("Please input your basis file name = ")
 
     if answer == 'y':
       total_random_number = int(input("How many random input do you want? "))
       random_heights = generate_random_number(total_random_number)
 
       for height in random_heights:
-          htr_fuel_height_replacer(height, read_file_name)
+          current_file_name = htr_fuel_height_replacer(height, basis_file_name)
+          title_replacer(height, current_file_name)
 
     else:
       height = int(input("Input Height of HTR Fuel in Core (cm) "))
-      htr_fuel_height_replacer(height, read_file_name)
+      current_file_name = htr_fuel_height_replacer(height, basis_file_name)
+      title_replacer(height, current_file_name)
 
 
 if __name__ == '__main__':
